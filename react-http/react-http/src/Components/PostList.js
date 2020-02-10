93e25a9f92a7bae9361c8fddc26e40f54daafc1b
ACPRAJ <60113453+ACPRAJ@users.noreply.github.com>
2020-02-10 10:39:14 -0700

    Changed the URL & Text box properties

--- react-http/react-http/src/Components/PostList.js
+++ react-http/react-http/src/Components/PostList.js
@@ -1,51 +1,54 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
 class PostList extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            posts: [],
-            errorMsg: ""
-        }
-    }
+    this.state = {
+      posts: [],
+      errorMsg: ""
+    };
+  }
 
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts1')
-            .then((response) => {
-                this.setState({ posts: response.data })
-            }
-            )
-            .catch((exception) => {
-                this.setState({ errorMsg: "Error while retrieving data" })
-                console.log(this.state.errorMsg)
-            })
-    }
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        this.setState({ posts: response.data });
+      })
+      .catch(exception => {
+        this.setState({ errorMsg: "Error while retrieving data" });
+        console.log(this.state.errorMsg);
+      });
+  }
 
-    render() {
-        const { posts, errorMsg } = this.state
-        return (
-            < div >
-                <h1>Post List</h1>
-                <div>
-                    {
-                        errorMsg ? <div> {errorMsg} </div> : null
-                    }
-                    {
-                        posts.length > 1 ?
-                            posts.map((post) =>
-                                <div key={'div' + post.id}>
-                                    <h3 id={'h' + post.id}>{post.title}</h3>
-                                    <input type="textarea" value={post.body}></input>
-                                </div>) :
-                            errorMsg ? null : <div>No data to display</div>
-                    }
-
-                </div>
-            </div >
-        )
-    }
+  render() {
+    const { posts, errorMsg } = this.state;
+    return (
+      <div>
+        <h1>Post List</h1>
+        <div>
+          {errorMsg ? <div> {errorMsg} </div> : null}
+          {posts.length > 1 ? (
+            posts.map(post => (
+              <div key={"div" + post.id}>
+                <h3 id={"h" + post.id}>{post.title}</h3>
+                <textarea
+                  rows="4"
+                  cols="75"
+                  readOnly={true}
+                  value={post.body}
+                ></textarea>
+              </div>
+            ))
+          ) : errorMsg ? null : (
+            <div>No data to display</div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default PostList
+export default PostList;
