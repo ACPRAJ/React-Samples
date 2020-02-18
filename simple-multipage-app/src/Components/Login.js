e7063d888ea2e605cdfe45739dd542cd24832ef7
ACPRAJ <60113453+ACPRAJ@users.noreply.github.com>
2020-02-18 10:58:24 -0700

    Centerd the login div both vertically and horizontally

--- simple-multipage-app/src/Components/Login.js
+++ simple-multipage-app/src/Components/Login.js
@@ -1,78 +1,95 @@
-import React, { Component } from 'react'
-import MainPage from './MainPage'
+import React, { Component } from "react";
+import MainPage from "./MainPage";
 
 class Login extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            username: "",
-            password: "",
-            isLoggedIn: false
-        }
+    this.state = {
+      username: "",
+      password: "",
+      isLoggedIn: false
+    };
+  }
+
+  onChangeHandler = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  onLoginButtonClicked = () => {
+    const { username, password } = this.state;
+
+    if (username == "acp" && password == "acp") {
+      this.setState({
+        isLoggedIn: true
+      });
+      console.log(this.props.history);
+      return;
     }
 
-    onChangeHandler = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+    alert("Invalid User Credentials");
+  };
 
-    onLoginButtonClicked = () => {
-        const { username, password } = this.state;
+  onResetButtonClicked = () => {
+    this.setState({
+      username: "",
+      password: ""
+    });
+  };
 
-        if (username == "acp" && password == "acp") {
-            this.setState({
-                isLoggedIn: true
-            })
-            console.log(this.props.history);
-            return;
-        }
+  componentDidMount() {
+    this.setState({
+      username: "acp",
+      password: "acp"
+    });
+  }
 
-        alert("Invalid User Credentials")
-    }
-
-    onResetButtonClicked = () => {
-        this.setState({
-            username: "",
-            password: ""
-        })
-    }
-
-    componentDidMount() {
-        this.setState({
-            username: "acp",
-            password: "acp"
-        })
-    }
-
-    render() {
-        if (this.state.isLoggedIn)
-            return (
-                <MainPage />
-            )
-        else
-            return (
-                <div className="login-div">
-                    <form>
-                        <div className="form-group">
-                            <label className="form-label">Username</label>
-                            <input className="form-control" type="text" name="username" id="txtUserName"
-                                onChange={this.onChangeHandler} value={this.state.username} />
-                        </div>
-                        <div className="form-group">
-                            <label className="form-label">Password</label>
-                            <input className="form-control" type="password" name="password"
-                                onChange={this.onChangeHandler} value={this.state.password} />
-                        </div>
-                        <div className="login-buttons-div" >
-                            <button className="btn btn-info" onClick={this.onLoginButtonClicked}>Login</button>
-                            <button className="btn btn-info" onClick={this.onResetButtonClicked}>Reset</button>
-                        </div>
-                    </form>
-                </div >
-            )
-    }
+  render() {
+    if (this.state.isLoggedIn) return <MainPage />;
+    else
+      return (
+        <div className="login-div">
+          <form>
+            <div className="form-group">
+              <label className="form-label">Username</label>
+              <input
+                className="form-control"
+                type="text"
+                name="username"
+                onChange={this.onChangeHandler}
+                value={this.state.username}
+              />
+            </div>
+            <div className="form-group">
+              <label className="form-label">Password</label>
+              <input
+                className="form-control"
+                type="password"
+                name="password"
+                onChange={this.onChangeHandler}
+                value={this.state.password}
+              />
+            </div>
+            <div className="login-buttons-div">
+              <button
+                className="btn btn-info btn-curved"
+                onClick={this.onLoginButtonClicked}
+              >
+                Login
+              </button>
+              <button
+                className="btn btn-info btn-curved"
+                onClick={this.onResetButtonClicked}
+              >
+                Reset
+              </button>
+            </div>
+          </form>
+        </div>
+      );
+  }
 }
 
-export default Login
+export default Login;
