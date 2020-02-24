1db97c0efd61ee9a5a78f207a4e52e9211a56a7d
ACPRAJ <60113453+ACPRAJ@users.noreply.github.com>
2020-02-24 15:26:32 -0700

    Fetch Post on a click of a button

--- hooks-samples/src/Components/Data-Fetching/FetchOnePost.js
+++ hooks-samples/src/Components/Data-Fetching/FetchOnePost.js
@@ -3,16 +3,16 @@ import axios from "axios";
 
 function FetchOnePost() {
   const [Post, setPost] = useState({});
-  const [Id, setId] = useState(1);
+  const [id, setId] = useState(1);
   const [IdFromButtonClick, setIdFromButtonClick] = useState(1);
 
   const fetchPost = () => {
-    if (Id <= 0) {
+    if (id <= 0) {
       setPost("");
       return;
     }
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${Id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then(response => {
         console.log(response);
         setPost(response.data);
@@ -20,9 +20,6 @@ function FetchOnePost() {
       .catch(exception => {
         console.log(exception);
       });
-    //   .finally(() => {
-    //     setCanFetch(false);
-    //   });
   };
 
   useEffect(fetchPost, [IdFromButtonClick]);
@@ -31,10 +28,10 @@ function FetchOnePost() {
     <div>
       <input
         type="text"
-        value={Id}
+        value={id}
         onChange={event => setId(event.target.value)}
       />
-      <button onClick={() => setIdFromButtonClick(Id)}>Fetch Post</button>
+      <button onClick={() => setIdFromButtonClick(id)}>Fetch Post</button>
       <h3>{Post.title}</h3>
     </div>
   );
