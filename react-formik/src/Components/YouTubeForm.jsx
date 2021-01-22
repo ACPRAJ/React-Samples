b0680485b783cc6e0371cf81fe9d3472f4feabfe
ACPRAJ <60113453+ACPRAJ@users.noreply.github.com>
2021-01-21 18:34:29 -0700

    Field with render props & error message with components

--- react-formik/src/Components/YouTubeForm.jsx
+++ react-formik/src/Components/YouTubeForm.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import ErrorText from "./ErrorText";
 
 const initialValues = {
   txtName: "",
   txtEMail: "",
   txtChannel: "",
+  txtComments: "",
+  txtAddress: "",
 };
 
 const onSubmit = (values) => {
@@ -18,6 +21,7 @@ const validationSchema = Yup.object({
     .required("E-Mail is required")
     .email("E-mail format is incorrect"),
   txtChannel: Yup.string().required("Channel is required"),
+  txtAddress: Yup.string().required("Address filed is required"),
 });
 
 function YouTubeForm() {
@@ -31,19 +35,59 @@ function YouTubeForm() {
         <div className="form-control">
           <label htmlFor="txtName">Name</label>
           <Field type="text" id="txtName" name="txtName"></Field>
-          <ErrorMessage name="txtName" />
+          <ErrorMessage name="txtName" component={ErrorText} />
         </div>
 
         <div className="form-control">
           <label htmlFor="txtEMail">E-Mail</label>
-          <Field type="email" id="txtEMail" name="txtEMail"></Field>
-          <ErrorMessage name="txtEMail" />
+          <Field
+            type="email"
+            id="txtEMail"
+            name="txtEMail"
+            placeholder="name@email.com"
+          ></Field>
+          <ErrorMessage name="txtEMail">
+            {(errorMsg) => <div className="error">{errorMsg}</div>}
+          </ErrorMessage>
         </div>
 
         <div className="form-control">
           <label htmlFor="txtChannel">Channel</label>
-          <Field type="text" id="txtChannel" name="txtChannel"></Field>
-          <ErrorMessage name="txtChannel" />
+          <Field
+            type="text"
+            id="txtChannel"
+            name="txtChannel"
+            placeholder="Youtube Channel"
+          ></Field>
+          <ErrorMessage name="txtChannel" component={ErrorText} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="txtComments">Comments</label>
+          <Field
+            as="textarea"
+            id="txtComments"
+            name="txtComments"
+            placeholder="Optional comments"
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="txtAddress">Address</label>
+          <Field name="txtAddress">
+            {(props) => {
+              const { field, form, meta } = props;
+              console.log("Props debug: ", props);
+              return (
+                <div>
+                  <input type="text" id="txtAddress" {...field} />
+                  {meta.touched && meta.error ? (
+                    <div className="error">{meta.error}</div>
+                  ) : null}
+                </div>
+              );
+            }}
+          </Field>
         </div>
 
         <button id="btnSubmit" type="submit">
